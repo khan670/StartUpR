60a7bfbe7ae94204658033c520ff804dc8547e49
Rayan khan <rayankhanfeg@gmail.com>
2025-01-03 10:52:00 -0800

    resolving issues

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -21,9 +21,11 @@ const Hero: React.FC = () => {
       >
         <div
           style={{
+            backgroundImage: "url('hero2-bg.webp')",
             backgroundPosition: "center center",
           }}
-          className="h-full relative bg-cover md:bg-[url('hero2-bg.webp')] bg-none"
+          // md:bg-[url('hero2-bg.webp')] bg-none
+          className="h-full relative bg-cover "
         >
           <div className="absolute sm:top-2/3 top-[22rem] sm:left-20 left-10 md:w-1/3 w-3/4  transform  -translate-y-1/2">
             <motion.h1
